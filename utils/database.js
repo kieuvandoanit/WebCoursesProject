8db5b8c62286340a0f7ff3b873ed708f4377b571
Duyên Võ Thị Ngọc <ngocduyenvothi.1509@gmail.com>
2022-01-13 20:56:23 +0700

    view user info

--- utils/database.js
+++ utils/database.js
@@ -2,8 +2,8 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "postgres",
-    database: "WebProject_covidManagement",
+    password: "12345",
+    database: "ProjectCovid",
     host: "localhost",
     port: 5432
 });
