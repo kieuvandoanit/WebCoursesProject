d6fc5e86abdcea2e03af9db40a00e0821136330e
Duyên Võ Thị Ngọc <ngocduyenvothi.1509@gmail.com>
2022-01-16 11:00:39 +0700

    user view history management

--- CovidManagerment/utils/database.js
+++ CovidManagerment/utils/database.js
@@ -3,10 +3,15 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user: "postgres",
 
+    //     password: "giang1712404",
+    //     database: "covidSystem",
+    password: "postgres",
+    database: "WebProject_covidManagement",
     // password: "giang1712404",
     // database: "covidSystem",
-    password: "123456",
-    database: "ProjectCovid",
+    //password: "123456",
+    //database: "ProjectCovid",
+
     host: "localhost",
     port: 5432
 });
